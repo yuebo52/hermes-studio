1a7af29ee1a1c581c3b428c57dd3bcb52c24965f
jiang yuebo <yuebo@qq.com>
2026-08-10 11:46:10 +0800

    Update LoginView.vue
    删除登录提示

--- packages/client/src/views/LoginView.vue
+++ packages/client/src/views/LoginView.vue
@@ -76,7 +76,6 @@ async function handlePasswordLogin() {
       </div>
       <h1 class="login-title">{{ t("login.title") }}</h1>
       <p class="login-desc">{{ t("login.description") }}</p>
-      <p class="login-default-hint">{{ t("login.defaultCredentialsHint") }}</p>
 
       <form class="login-form" @submit.prevent="handleLogin">
         <input
